69b19393cbccfc5a4ad8eb1c1964ae2027243cd0
Hayden <haydos.shaw@gmail.com>
2022-05-02 15:58:44 +1200

    test

--- front-page.php
+++ front-page.php
@@ -76,7 +76,7 @@
     <div class="activities-section-container">
         <p class="subheader"><i class="fa-light fa-person-biking"></i>Things to do</p>
         <p class="header-1">Activities around Roxburgh</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>This is an update.... WIll it auto push????Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <div class="activities-grid">
             <div class="activities-grid-column">
                 <div class="activity">
